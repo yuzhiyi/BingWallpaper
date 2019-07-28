291cb4dddf2cdc1f0aae73a03d2d092b437c3fbb
chenwenyu <chenwenyu@sys-in.com.cn>
2019-07-28 15:39:23 +0800

    fix(config):更新 config 注释说明

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 // 配置文件
 module.exports = {
-    DAILYSENDDATE: '0 0 8 * * *',
-    SENDDATE: '10 * * * * *', //定时请求更换壁纸，规则见 /schedule/index.js
-    OVERDAYS: 10 // 删除超过时间OVERDAYS的文件
+    DAILYSENDDATE: '0 0 8 * * *', // 定时获取最新壁纸时间，规则见 /schedule/index.js
+    SENDDATE: '10 * * * * *', // 定时更换壁纸时间，规则见 /schedule/index.js
+    OVERDAYS: 10 // 删除超过时间 OVERDAYS 的文件
 }
